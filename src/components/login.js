d32f3e7f9677f9bdafc081da197334313a63ff33
Lorenzo <io.lorenzo.gaspari@gmail.com>
2023-04-20 22:51:20 +0200

    loginpo

--- src/components/login.js
+++ src/components/login.js
@@ -2,6 +2,9 @@ import { GoogleLogin, GoogleOAuthProvider} from '@react-oauth/google';
 
 const clientID = "405532619153-fdd53vgtreamje4resjdt4cgcehnj41j.apps.googleusercontent.com";
 
+
+// 104215956377-cmhsl8dngd83fj6c673j9jhjot03qkhd.apps.googleusercontent.com
+
 function Login(){
 
     const onSuccess = (res) => {
@@ -13,10 +16,9 @@ function Login(){
     }
 
     return(
-        <GoogleOAuthProvider clientId="104215956377-cmhsl8dngd83fj6c673j9jhjot03qkhd.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId={clientID}>
             <div id='signInButton'>
                 <GoogleLogin
-                clientId={clientID}
                 buttonText='Login'
                 onSuccess={onSuccess}
                 onFailure={onFailure}
